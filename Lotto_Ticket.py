914a2d20728aac0cb11f96591fe6d1241c18fd1c
JaxonPittmn <159969578+JaxonPittmn@users.noreply.github.com>
2025-07-15 14:46:04 -0500

    Add files via upload

---
+++ Lotto_Ticket.py
@@ -0,0 +1,61 @@
+
+import os, sys
+
+file = open("tick_nums.txt", 'r')
+tickSheet = file.read()
+
+tickSheet = tickSheet.split("\n")
+
+
+lottos=[]
+
+ended = False
+
+for ticket in tickSheet:
+    ticket = ticket.split(" ")
+    numt=[]
+    for item in ticket:
+        numt.append(int(item))
+    lottos.append(numt)
+
+while True:
+    tim=0
+    possible=[]
+    ended=False
+    print("New Ticket Set")
+
+    while not ended and tim!=6:
+        remove = []
+        if tim!=0 and len(possible)==0:
+            
+            print("No Tickets")
+            ended=True
+            break
+        
+        tickn=int(input("Select> "))
+        print("\n")
+        
+        if tim==0:
+            for i,ticket in enumerate(lottos):
+                if tickn in ticket:
+              
+                    possible.append(lottos[i])
+                    
+        else:
+            for i,ticket in enumerate(possible):
+                if tickn not in ticket:
+                    remove.append(ticket)
+ 
+        for item in remove:
+            possible.remove(item)
+
+        if len(possible) != 0:
+            print("Tickets left:")
+            for ticket in possible:
+                print(ticket)
+        
+        print("\n")
+        tim += 1
+        if tim==6:
+            print(f"Winning Ticket: {possible[0]}")
+            sys.exit()
